76293b078ddfa2b42fbe635c341d01196e16813c
royhansuhaili <113328436+royhansuhaili@users.noreply.github.com>
2022-09-14 14:13:26 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan";
+        echo"Nama saya Royhan Suhaili
+        saya tinggal di daerah tambun selatan
+        olah raga yang saya sukai adalah basket";
+    }
+}
+
+?>
